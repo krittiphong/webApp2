25905f5e076853fdd5489ca8b74923ee0cd3a25c
tey_k_000 <b.krittiphong@gmail.com>
2017-03-06 15:56:34 +0700

    test

---
+++ controller/manage.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tey_k_000
+ * Date: 3/6/2017
+ * Time: 10:50 AM
+ */ include_once ("../model/connect.php");
+    include_once ("../model/db_user.inc.php");
+    if(isset($_POST["delete"])) {
+        $id = $_POST["delete"];
+        echo $id;
+        $de = delete_user($id,$conn);
+    }
+    if(isset($_POST["save"])){
+        $user_update = array();
+        $user_update = $_POST["user"];
+        update_user($user_update,$conn);
+        //header("location: ../view/manage_user.php");
+    }
+    echo "5555";
+?>
